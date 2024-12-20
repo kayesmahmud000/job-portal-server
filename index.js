03ab6c6d975411e0c6ee4955f7974f87557de205
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2024-12-20 10:36:28 +0600

    added the functionality

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken')
  const cors = require('cors');
+ const cookieParser = require('cookie-parser');
+
  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
  const port = process.env.PORT || 5000
  const app =express()
 
  app.use(cors())
  app.use(express.json())
+ app.use(cookieParser())
 
 
  
@@ -26,9 +30,27 @@ const express = require('express');
      // Connect the client to the server	(optional starting in v4.7)
      await client.connect();
      const jobCollection = client.db("JobPortalDb").collection("Jobs");
+     const jobApplicationCollection = client.db("JobPortalDb").collection("applications");
+
+    //  auth api
+     app.post("/jwt", async(req, res)=>{
+      const user =req.body
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn:"1h"})
+      res.cookie('token', token,{
+        httpOnly: true,
+        secure:false
+
+      })
+      .send({success:true})
+     })
 
      app.get("/allJobs", async(req ,res)=>{
-        const cursor= jobCollection.find()
+      const email= req.query.email
+      let query={}
+      if(email){
+        query={hr_email: email}
+      }
+        const cursor= jobCollection.find(query)
        const  result= await cursor.toArray()
        res.send(result)
      })
@@ -39,6 +61,78 @@ const express = require('express');
       const result = await jobCollection.findOne(query) 
       res.send(result)
      })
+
+     app.post("/jobs", async(req, res)=>{
+      const newJob =req.body
+      const result= await jobCollection.insertOne(newJob)
+      res.send(result)
+     })
+
+     app.get("/job-application", async(req, res)=>{
+      const email= req.query.email
+      const query= {applicant_email: email}
+        const result= await jobApplicationCollection.find(query).toArray()
+
+        for(const apply of result){
+          const id = apply.job_id
+          const query1= {_id:new ObjectId(id)}
+          const result1 = await jobCollection.findOne(query1) 
+          if(result1){
+            apply.title= result1.title,
+            apply.company= result1.company
+            apply.company_logo= result1.company_logo
+          }
+        }
+        res.send(result)
+     })
+
+     app.get("/job-application/job/:id", async(req, res)=>{
+      const jobId= req.params.id
+      const query= {job_id: jobId}
+
+      const result=await jobApplicationCollection.find(query).toArray()
+      res.send(result)
+     })
+     
+      app.post("/job-application", async(req, res)=>{
+        const application= req.body
+        console.log(application)
+        const result= await jobApplicationCollection.insertOne(application)
+
+        const id =application.job_id;
+        const query= {_id :new ObjectId(id)}
+
+        const job = await jobCollection.findOne(query)
+        let newCount =0
+        if(job.applicationCount){
+          newCount= job.applicationCount+1
+        }else{
+          newCount=1
+        }
+        const filter= {_id: new ObjectId(id)}
+        const updateJob={
+          $set:{
+             applicationCount: newCount
+            }
+        }
+        const updateResult = await jobCollection.updateOne(filter, updateJob)
+        console.log(job)
+        res.send(result)
+
+      })
+      app.patch("/job-application/:id", async(req, res)=>{
+        const id = req.params.id
+        const filter={ _id: new ObjectId(id)}
+        const data = req.body
+
+        const updateDoc= {
+          $set:{
+            status: data.status
+          }
+        }
+        const result= await jobApplicationCollection.updateOne(filter, updateDoc)
+        res.send(result)
+      })
      
      // Send a ping to confirm a successful connection
      await client.db("admin").command({ ping: 1 });
